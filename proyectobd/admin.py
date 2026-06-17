69525ecf2686ad38a9f28e4ca00ec5b0785edba0
joaquinsotelo <joaquin.sotelo2000@gmail.com>
2026-06-17 10:49:39 -0700

    Se registraron los modelos en admin

--- proyectobd/admin.py
+++ proyectobd/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Sala, Requerimiento, Reservacion, HistorialReservacion
 
-# Register your models here.
+
+@admin.register(Sala)
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'capacidad', 'orden', 'activa')
+    list_filter = ('activa',)
+    search_fields = ('nombre',)
+
+
+@admin.register(Requerimiento)
+class RequerimientoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre',)
+
+
+@admin.register(Reservacion)
+class ReservacionAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre_evento',
+        'nombre_registra',
+        'fecha_evento',
+        'hora_inicio',
+        'hora_fin',
+        'asistentes',
+        'estatus',
+        'fecha_creacion',
+    )
+    list_filter = ('estatus', 'fecha_evento', 'salas')
+    search_fields = ('nombre_evento', 'nombre_registra')
+    filter_horizontal = ('salas', 'requerimientos')
+
+
+@admin.register(HistorialReservacion)
+class HistorialReservacionAdmin(admin.ModelAdmin):
+    list_display = ('reservacion', 'usuario', 'accion', 'fecha_accion')
+    list_filter = ('accion', 'fecha_accion')
+    search_fields = ('reservacion__nombre_evento', 'usuario__username', 'accion')
